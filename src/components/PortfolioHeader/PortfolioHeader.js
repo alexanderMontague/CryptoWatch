8d8d22cb3094b3260086c45054144489247dba8e
Alex Montague <xturnermonty@gmail.com>
2018-09-10 17:59:37 -0400

    Finalized style and setup for portfolio header details

--- src/components/PortfolioHeader/PortfolioHeader.js
+++ src/components/PortfolioHeader/PortfolioHeader.js
@@ -5,16 +5,16 @@ const PortfolioHeader = () => {
   return (
     <div className={css.portfolioHeaderContainer}>
       <span className={css.headerItem}>
-        Total Portfolio Value
-        <div className={css.portfolioValue}>$VALUE</div>
+        <div className={css.headerTitle}>Total Portfolio Value</div>
+        <div className={css.portfolioValue}>$1234.50</div>
       </span>
       <span className={css.headerItem}>
-        24hr Change
-        <div className={css.dayPercentChange}>%VALUE</div>
+        <div className={css.headerTitle}>24hr Change</div>
+        <div className={css.dayPercentChange}>+ $5.00 (3.56%)</div>
       </span>
       <span className={css.headerItem}>
-        Total Portfolio Gain
-        <div className={css.totalPortfolioGain}>$VALUE</div>
+        <div className={css.headerTitle}>Total Portfolio Gain</div>
+        <div className={css.totalPortfolioGain}>- $10.45 (-12.69%)</div>
       </span>
     </div>
   );
