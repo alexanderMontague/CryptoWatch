4bb25b224b89294cc89c30da6f7d7b3f619c605f
Alex Montague <xturnermonty@gmail.com>
2018-09-10 09:16:20 -0400

    Started to create and style Portfolio Header banner

--- src/components/HeaderItem/HeaderItem.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import css from './HeaderItem.scss';
-
-const HeaderItem = props => {
-  const { itemTitle } = props;
-  return <div className={css.headerItemContainer}>{itemTitle}</div>;
-};
-
-export default HeaderItem;
--- src/components/PortfolioHeader/PortfolioHeader.js
+++ src/components/PortfolioHeader/PortfolioHeader.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import css from './PortfolioHeader.scss';
 
-import HeaderItem from '../HeaderItem/HeaderItem';
-
 const PortfolioHeader = () => {
-  return <div className={css.portfolioHeaderContainer}>wassup</div>;
+  return (
+    <div className={css.portfolioHeaderContainer}>
+      <span className={css.headerItem}>
+        Total Portfolio Value
+        <div className={css.portfolioValue}>$VALUE</div>
+      </span>
+      <span className={css.headerItem}>
+        24hr Change
+        <div className={css.dayPercentChange}>%VALUE</div>
+      </span>
+      <span className={css.headerItem}>
+        Total Portfolio Gain
+        <div className={css.totalPortfolioGain}>$VALUE</div>
+      </span>
+    </div>
+  );
 };
 
 export default PortfolioHeader;
