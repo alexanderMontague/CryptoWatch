f3da079ac8180ad2957dcb0fc8ade2f4733413f9
Alex Montague <xturnermonty@gmail.com>
2018-11-12 17:24:32 -0500

    Add register form css

--- React Frontend/src/components/Slider/Slider.js
+++ React Frontend/src/components/Slider/Slider.js
@@ -4,12 +4,15 @@ import { connect } from 'react-redux';
 import { toggleMenu } from '../../actions';
 
 const Slider = props => {
-  const { showMenu } = props;
-  let menuSliderStyle = [css.menuModal, css.Close];
+  const { showMenu, isLoggedIn = false } = props; // TODO auth in redux
+  const menuSliderStyle = showMenu
+    ? [css.menuModal, css.Open]
+    : [css.menuModal, css.Close];
 
-  if (showMenu) {
-    menuSliderStyle = [css.menuModal, css.Open];
-  }
+  const amountChangeHandler = input => {
+    console.log('From: ' + input.name, input.value);
+  };
+
   return (
     <Fragment>
       <div
@@ -17,15 +20,98 @@ const Slider = props => {
         className={showMenu ? css.backgroundShadow : null}
       >
         <div onClick={props.toggleMenu} className={menuSliderStyle.join(' ')}>
-          <div className={css.menuLayer}>
-            user profile pic
-            <button>Settings</button>
-            <br />
-            <button>TBD</button>
-            <br />
-            <button>Sign Out</button>
-            <br />
-          </div>
+          {isLoggedIn ? (
+            <div className={css.menuLayer}>
+              user profile pic
+              <button>Settings</button>
+              <br />
+              <button>TBD</button>
+              <br />
+              <button>Sign Out</button>
+              <br />
+            </div>
+          ) : (
+            <div className={css.menuLayer}>
+              <h3>Create an Account!</h3>
+              <form
+                className={css.addCoinForm}
+                onSubmit={() => console.log('submitted')}
+              >
+                <label>
+                  Email:
+                  <input
+                    type="text"
+                    placeholder="Enter an Email"
+                    name="email"
+                    onChange={input => amountChangeHandler(input.target)}
+                    className={
+                      // renderPriceRequire
+                      //   ? [css.addFormInput, css.requiredBorder].join(' ')
+                      //   : css.addFormInput
+                      css.addFormInput
+                    }
+                  />
+                </label>
+                <label>
+                  Username:
+                  <input
+                    className={
+                      // renderPriceRequire
+                      //   ? [css.addFormInput, css.requiredBorder].join(' ')
+                      //   : css.addFormInput
+                      css.addFormInput
+                    }
+                    type="text"
+                    placeholder="Enter a username"
+                    name="username"
+                    onChange={input => amountChangeHandler(input.target)}
+                  />
+                </label>
+                <label>
+                  Password:
+                  <input
+                    className={
+                      // renderPriceRequire
+                      //   ? [css.addFormInput, css.requiredBorder].join(' ')
+                      //   : css.addFormInput
+                      css.addFormInput
+                    }
+                    type="password"
+                    placeholder="Enter a password"
+                    name="passwordOne"
+                    onChange={input => amountChangeHandler(input.target)}
+                  />
+                </label>
+                <label>
+                  Re-enter Password:
+                  <input
+                    className={
+                      // renderPriceRequire
+                      //   ? [css.addFormInput, css.requiredBorder].join(' ')
+                      //   : css.addFormInput
+                      css.addFormInput
+                    }
+                    type="password"
+                    placeholder="Enter same password!"
+                    name="passwordTwo"
+                    onChange={input => amountChangeHandler(input.target)}
+                  />
+                </label>
+                <button
+                  className={css.addButton}
+                  type="submit"
+                  // disabled={
+                  //   renderDateRequire ||
+                  //   renderPriceRequire ||
+                  //   renderAmountRequire ||
+                  //   !dataAvailable
+                  // }
+                >
+                  Register!
+                </button>
+              </form>
+            </div>
+          )}
         </div>
       </div>
     </Fragment>
