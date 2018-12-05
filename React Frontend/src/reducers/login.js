e746d2aed434976b2711b7c759b7a4b3ca0a8713
Tim <timgibson49@gmail.com>
2018-12-05 15:10:41 -0500

    feat: Signup portion

--- React Frontend/src/reducers/login.js
+++ React Frontend/src/reducers/login.js
@@ -7,6 +7,9 @@ const authReducer = (prevState=initialState, action) => {
   switch (action.type) {
     case 'LOGIN_SUCCESS':
       return { ...prevState, loggedIn: true };
+    case 'SIGNUP_SUCCESS':
+      console.log(action.payload);
+      return prevState;
     case 'OPEN_LOGIN_MODAL':
       return { ...prevState, loginModalOpen: true };
     case 'CLOSE_LOGIN_MODAL':
