4453f8a00eaba0348837704567552c3f168a2992
alexanderMontague <xturnermonty@gmail.com>
2018-12-08 17:31:37 -0500

    Refactor actions

--- Node Server/app.js
+++ Node Server/app.js
@@ -72,7 +72,7 @@ app.use(
     resave: true,
     saveUninitialized: true,
     secret: process.env.SESSION_SECRET,
-    cookie: { maxAge: 1209600000 }, // two weeks in milliseconds
+    cookie: { maxAge: 86000000 }, // one day
     // store: new MongoStore({
     //   url: process.env.MONGODB_URI,
     //   autoReconnect: true,
