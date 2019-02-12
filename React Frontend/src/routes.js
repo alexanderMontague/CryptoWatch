8ed82d9658b6110720aa19b8dadebea8f85c2587
alexanderMontague <xturnermonty@gmail.com>
2019-02-12 17:30:28 -0500

    update css

--- React Frontend/src/routes.js
+++ React Frontend/src/routes.js
@@ -4,17 +4,21 @@ import { Switch, Route, Redirect } from 'react-router';
 import Layout from './containers/Layout/Layout';
 import LoginPage from './containers/LoginModal';
 
-
 const fakeAuth = {
   isAuthenticated: false
 };
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
-  <Route {...rest} render={(props) => (
-    fakeAuth.isAuthenticated === true
-      ? <Component {...props} />
-      : <Redirect to='/login' />
-  )} />
+  <Route
+    {...rest}
+    render={props =>
+      fakeAuth.isAuthenticated === true ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to="/login" />
+      )
+    }
+  />
 );
 
 const routes = (
